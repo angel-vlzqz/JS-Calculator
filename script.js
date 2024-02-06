4e4e3443d356ed7480e34ef5edbb8a2e13130fcf
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-02-06 16:00:30 -0700

    refactor python code for expression evaluation

---
+++ script.js
@@ -0,0 +1,148 @@
+function precedence(op)
+{
+
+  if (op == '+' || op == '-')
+  {
+    return 1;
+  }
+  if (op == '*' || op == '/' || op == '^')
+  {
+    return 2;
+  }
+  return 0;
+}
+
+// Function to perform arithmetic
+// operations.
+  function applyOp(a, b, op)
+{
+
+  if (op == '+') return a + b;
+  if (op == '-') return a - b;
+  if (op == '*') return a * b;
+  if (op == '/') return a / b;
+  if (op == '^') return a ** b;
+}
+
+// Function that returns value of
+// expression after evaluation.
+  function evaluate(tokens)
+{
+
+  // stack to store integer values.
+    let values = [];
+
+  // stack to store operators.
+    let ops = [];
+  let i = 0;
+
+  while (i < len(tokens))
+  {
+
+    // Current token is a whitespace,
+      // skip it.
+      if (tokens[i] == ' ')
+    {
+      i += 1;
+      continue
+    }
+
+    // Current token is an opening 
+    // brace, push it to 'ops'
+    else if (tokens[i] == '(')
+      {
+        ops.append(tokens[i]);
+      }
+
+      // Current token is a number, push 
+      // it to stack for numbers.
+      else if (tokens[i].isdigit())
+      {
+        let val = 0;
+
+        // There may be more than one
+        // digits in the number.
+          while (i < len(tokens) && tokens[i].isdigit())
+        {
+          val = (val * 10) + int(tokens[i]);
+          i += 1;
+        }
+        values.append(val);
+
+        // right now the i points to 
+        // the character next to the digit,
+          // since the for loop also increases 
+        // the i, we would skip one 
+        // token position; we need to 
+        // decrease the value of i by 1 to
+        // correct the offset.
+          i-=1;
+      }
+
+      // Closing brace encountered, 
+      // solve entire brace.
+      else if (tokens[i] == ')')
+    {
+      while (len(ops) != 0 && ops[-1] != '(')
+        {
+          let val2 = values.pop();
+          let val1 = values.pop();
+          let op = ops.pop();
+
+          values.append(applyOp(val1, val2, op));
+        }
+
+        // pop opening brace.
+        ops.pop()
+      }
+
+    // Current token is an operator.
+      else
+    {
+      // While top of 'ops' has same or 
+      // greater precedence to current 
+      // token, which is an operator. 
+        // Apply operator on top of 'ops' 
+      // to top two elements in values stack.
+        while (len(ops) != 0 && precedence(ops[-1]) >= precedence(tokens[i]))
+      {
+
+        let val2 = values.pop();
+        let val1 = values.pop();
+        let op = ops.pop();
+
+        values.append(applyOp(val1, val2, op));
+      }
+
+      // Push current token to 'ops'.
+        ops.append(tokens[i]);
+    }
+    i += 1;
+  }
+
+  // Entire expression has been parsed 
+  // at this point, apply remaining ops 
+  // to remaining values.
+    while (len(ops) != 0)
+  {
+    let val2 = values.pop();
+    let val1 = values.pop();
+    let op = ops.pop();
+
+    values.append(applyOp(val1, val2, op));
+  }
+  // Top of 'values' contains result,
+    // return it.
+    return values[-1];
+}
+
+// Driver Code
+
+console.log(evaluate("10 + 2 * 6"))
+console.log(evaluate("100 * 2 + 12"))
+console.log(evaluate("100 * ( 2 + 12 )"))
+console.log(evaluate("100 * ( 2 + 12 ) / 14"))
+console.log(evaluate("9 ^ 3"))
+console.log(evaluate("9 ^ 3"))
+console.log(evaluate("(2+1)^2"))
+
